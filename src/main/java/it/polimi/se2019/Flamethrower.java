e79ebb4aa922a7af5fccd7fed3bb6a2da1b06ebc
Chiara Marzano <chiara.marzano@mail.polimi.it>
2019-03-30 18:05:37 +0100

    Added classes for weapons and weaponspot.

---
+++ src/main/java/it/polimi/se2019/Flamethrower.java
@@ -0,0 +1,21 @@
+package it.polimi.se2019;
+
+import it.polimi.se2019.model.Action;
+import it.polimi.se2019.model.Pair;
+import it.polimi.se2019.model.Weapon;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Flamethrower extends Weapon {
+    @Override
+    public Pair<List<Map<Figure, List<Action>>>, List<Ammo>> effect(Figure figure, List<Map<Figure, List<Action>>> storedMoves) {
+        return null;
+    }
+
+    @Override
+    public Set<Figure> generateTargetSet(List<Map<Figure, List<Action>>> storedMoves) {
+        return null;
+    }
+}
