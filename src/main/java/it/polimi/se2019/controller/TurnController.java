4c1a60a49990de9b09b4a4c9d91f98aabdc2e889
massimiliano.nigro <massimiliano.nigro@mail.polimi.it>
2019-06-27 15:41:38 +0200

    Minor changes

--- src/main/java/it/polimi/se2019/controller/TurnController.java
+++ src/main/java/it/polimi/se2019/controller/TurnController.java
@@ -52,7 +52,8 @@ public class TurnController extends Controller {
     @Override
     public void update(VCEvent message) {
         try {
-            message.handle(this);
+            if(message.getSource().equals(currentPlayer))
+                message.handle(this);
         }catch (UnsupportedOperationException e){
             //ignore events that this controller does not support
             Log.fine("TurnController ignored " + JsonHandler.serialize(message));
